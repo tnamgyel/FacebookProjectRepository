ddb55fe48a169703d8e16dde2ee095cc5d608f31
Tenyang <Tenyang@Tenyang>
2016-10-10 01:26:32 -0400

    moving files to github

---
+++ Github_Project/src/Facebook_Project.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook_Project {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		System.out.println("Facebook page is open");
+
+	}
+
+}
